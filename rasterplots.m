9603772ec4b08708a78c887b7c352ffd089cbdca
Leora <85642780+lwils72@users.noreply.github.com>
2023-03-20 17:15:22 -0500

    Update rasterplots.m

--- rasterplots.m
+++ rasterplots.m
@@ -94,12 +94,29 @@ colormap(cpolar);
 
 %dz2_2=-sqrt(dz_x(:,2:end).^2+dz1_x(2:end,:).^2); % says incompatible sizes but they are the same? ugh
 
-XDiff = dz1_x-dz_x;
+
+zDiff=(z-z1); % Ugh x1=before, x=after
 
 figure (9),clf, hold on
-title('XDiff') % titled for no confusion 
-imagesc(x/1e3,y/1e3,XDiff);
+title('zDiff') % titled for no confusion 
+imagesc(x/1e3,y/1e3,zDiff);
 axis xy; axis equal; grid; colorbar;
 caxis([-0.75,0.75]); % more defined axis
 colormap(cpolar); % using gray to give good shadows 
 
+
+zDiff=(z(:,2:end)-z1(:,2:end)); % Ugh 1=before, 'blank'=after
+% This just ends up giving me all zeros and NaNs even with '(:,2:end)'
+% to index ughhhhh nuggets
+
+figure (9),clf, hold on
+title('zDiff') % titled for no confusion 
+imagesc(x/1e3,y/1e3,zDiff(:,2:end));
+axis xy; axis equal; grid; colorbar;
+caxis([-0.5,0.5]); % more defined axis
+colormap(cpolar); % using gray to give good shadows 
+
+% What if I difference the shadowing data?
+
+dz_2Diff=(dz_x-dz1_x)
+% Still gives all zeros ???
